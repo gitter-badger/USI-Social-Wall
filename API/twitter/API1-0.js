84652b61ca1aba301c8ec1a73cb1e32c3e143605
calzam <marco.calzana@yahoo.it>
2015-11-24 10:40:08 +0100

    config js for twitter API 1.0

--- API/twitter/API1-0.js
+++ API/twitter/API1-0.js
@@ -10,25 +10,16 @@ var twitterRouter = require("../../routes/twitter/router")
 
 
 module.exports.getJeson = function(res, hashtag){
-	// console.log(req, res)
  // console.log('check','https://api.twitter.com/1.1/search/tweets.json?q='+hashtag.replace(/#/g,'%23'))
- var oauth = new OAuth.OAuth(
- 'https://api.twitter.com/oauth/request_token',
- 'https://api.twitter.com/oauth/access_token',
- 'ohb9GbS1FS1zGxwjz1LvF4sQR',
- 'Z3jc8wKNooT82QfMfQBkpoDegJGEcQnCwwKXfhh8iFauhe15F5',
- '1.0A',
- null,
- 'HMAC-SHA1'
- );
- oauth.get(
- 'https://api.twitter.com/1.1/search/tweets.json?q='+hashtag.replace(/#/g,'%23')+"&count=20",
- '4226994315-eDbzpMKJ6rOTNBP24n9cvaAOpxK5QaOh1dZzAkP',
- 'nUB3f2yCpz3VRVAgCttiS1r1UyNsZxddhzOJ5nt3evckr',
- function (e, data, response) {
- 	if (e) console.log(e)
- 	twitterRouter.sendData(res, data);
- });
+	var oauth = config.twitter1.oauth1;
+	oauth.get(
+		'https://api.twitter.com/1.1/search/tweets.json?q='+hashtag.replace(/#/g,'%23')+"&count=20",
+		config.twitter1.standardKey,
+		config.twitter1.standardValue,
+		function (e, data, response) {
+			if (e) console.log(e)
+			twitterRouter.sendData(res, data);
+		});
 
 }
 
--- config.js
+++ config.js
@@ -1,9 +1,29 @@
+var OAuth = require('oauth');
+
 module.exports = {
-  // MongoDB
-  // Url of the Mongodb server
-  mongoUrl: "mongodb://localhost/",
-  // Database name
-  mongoDbName: "test-2",
-  //Server URL
-  url: "http://127.0.0.1:3000"
+  	// MongoDB
+  	// Url of the Mongodb server
+	mongoUrl: "mongodb://localhost/",
+ 	// Database name
+  	mongoDbName: "test-2",
+  	//Server URL
+  	url: "http://127.0.0.1:3000",
+
+  	//oauth for Twitter API 1.0
+  	twitter1 : {
+		oauth1 : new OAuth.OAuth(
+			'https://api.twitter.com/oauth/request_token',
+			'https://api.twitter.com/oauth/access_token',
+			'ohb9GbS1FS1zGxwjz1LvF4sQR',
+			'Z3jc8wKNooT82QfMfQBkpoDegJGEcQnCwwKXfhh8iFauhe15F5',
+			'1.0A',
+			null,
+			'HMAC-SHA1'
+	 	),
+	 	standardKey : '4226994315-eDbzpMKJ6rOTNBP24n9cvaAOpxK5QaOh1dZzAkP',
+	 	standardValue :'nUB3f2yCpz3VRVAgCttiS1r1UyNsZxddhzOJ5nt3evckr'
+	}
 }
+
+
+//env
